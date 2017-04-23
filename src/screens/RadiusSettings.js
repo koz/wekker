c9b180ad67f1c8f712c2ecb1753e71c7dcfd3629
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-04-23 15:06:29 -0300

    Change Slider component

--- src/screens/RadiusSettings.js
+++ src/screens/RadiusSettings.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import {
   View,
-  Slider,
   Text,
   StyleSheet,
   Button,
 } from 'react-native'
+import Slider from 'react-native-slider'
 
 class RadiusSettings extends Component {
   static navigationOptions = {
@@ -27,11 +27,15 @@ class RadiusSettings extends Component {
     return (
       <View style={styles.container}>
         <Slider
+          value={this.state.value}
           maximumValue={2000}
           minimumValue={500}
           step={100}
           onValueChange={this.handleSlide}
           style={styles.slider}
+          trackStyle={styles.track}
+          thumbStyle={styles.thumb}
+          minimumTrackTintColor='#d14ba6'
         />
         <Text style={styles.text}>
           {`${this.state.value} m`}
@@ -50,9 +54,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#FFFFFF',
     paddingBottom: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
-  slider: {
-    marginBottom: 20,
+  track: {
+    height: 10,
+    borderRadius: 4,
+    backgroundColor: '#ADD8E6',
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 1},
+    shadowRadius: 1,
+    shadowOpacity: 0.15,
+  },
+  thumb: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#f8a1d6',
+    borderColor: '#a4126e',
+    borderWidth: 5,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 2,
+    shadowOpacity: 0.35,
+    top: 25,
   },
   text: {
     alignSelf: 'center',
