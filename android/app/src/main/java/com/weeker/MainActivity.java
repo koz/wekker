3707de99d9db4d3728ef55ac73671ad27cb295c8
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-06-08 23:34:19 -0300

    Rename project to Weeker

--- android/app/src/main/java/com/weeker/MainActivity.java
+++ android/app/src/main/java/com/weeker/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mapproject;
+package com.weeker;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "MapProject";
+        return "Weeker";
     }
 }
