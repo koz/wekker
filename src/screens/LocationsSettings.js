5b1c351be899c030faef9162e8cc9f1bf700225d
leandrooriente <leandroriente@gmail.com>
2017-06-18 23:06:54 -0300

    WIP

--- src/screens/LocationsSettings.js
+++ src/screens/LocationsSettings.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
-import Title from '../components/Title'
 import settingsIcon from '../assets/settings.png'
+import arrowIcon from '../assets/arrow.png'
+import { navigationStyles } from '../utils/header'
 import {
   View,
   StyleSheet,
-  Image
+  TouchableHighlight,
+  Animated,
+  Image,
+  Easing,
+  Text
 } from 'react-native'
+
+const Item = title => ({
+  title,
+  value: null,
+  opened: false,
+  angle: new Animated.Value(90)
+})
 
 class LocationsSettings extends Component {
   static navigationOptions = {
-    header: () => (
-      <Title text={'Definir localizações'} />
-    ),
+    title: 'Definir localização',
+    ...navigationStyles,
     tabBarIcon: (focused, tintColor) => (
       <Image
         source={settingsIcon}
@@ -20,18 +31,110 @@ class LocationsSettings extends Component {
     )
   }
 
+  state = {
+    list: [
+      new Item('Casa'),
+      new Item('Trabalho')
+    ]
+  }
+
+  handleLocationClick = index => {
+    this.setState(prevState => ({
+      list: prevState.list.map((l, i) => {
+        if (i === index) {
+          return { ...l, opened: !l.opened }
+        }
+
+        return { ...l }
+      })
+    }), () => {
+      this.animate(index, this.state.list[index].opened)
+    })
+  }
+
+  animate = (index, reverse) => {
+    Animated.timing(this.state.list[index].angle, {
+      toValue: reverse ? 270 : 90,
+      duration: 200,
+      easing: Easing.linear
+    }).start();
+  }
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        {
+          this.state.list.map((l, i) => (
+            <View key={i} style={styles.itemContainer}>
+              <TouchableHighlight
+                underlayColor={'#FFF'}
+                onPress={() => this.handleLocationClick(i)}
+                style={styles.button}
+              >
+                <View style={styles.buttonWrapper}>
+                  <Text style={styles.buttonText}>
+                    { l.title.toUpperCase() }
+                  </Text>
+                  <Animated.Image
+                    style={[
+                      styles.arrowIcon,
+                      { transform: [{
+                        rotate: this.state.list[i].angle.interpolate({
+                          inputRange: [0, 360],
+                          outputRange: ['0deg', '360deg']
+                        })
+                      }]}
+                    ]}
+                    source={arrowIcon}
+                  />
+                </View>
+              </TouchableHighlight>
+            </View>
+          ))
+        }
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    paddingTop: 20,
+    backgroundColor: '#FFF',
+    paddingBottom: 40,
+  },
+  itemContainer: {
+    marginLeft: 30,
+    marginRight: 30,
+    borderBottomWidth: 1,
+    borderColor: '#CCC'
+  },
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    paddingTop: 30,
+    paddingBottom: 30,
+    backgroundColor: '#FFF',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  arrowIcon: {
+    width: 16,
+    height: 16,
+    alignSelf: 'flex-end',
+    marginRight: 10,
+    transform: [{
+      rotate: '90deg'
+    }]
+  },
   settingsIcon: {
     width: 26,
-    height: 26,
+    height: 26
   }
 })
 
---
+++ src/utils/header.js
@@ -0,0 +1,16 @@
+export const navigationStyles = {
+  headerStyle: {
+    height: 100,
+    padding: 0,
+    backgroundColor: '#FFF',
+    borderBottomWidth: 1,
+    borderColor: '#CCC'
+  },
+  headerTitleStyle: {
+    fontSize: 28,
+    color: 'black',
+    padding: 30,
+    marginHorizontal: 0,
+    fontWeight: 'bold'
+  }
+}
