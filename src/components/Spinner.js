50d581845ddf204707bf39d5bd32b20b69dce3ae
ohmyguigs <ohmyguigs@gmail.com>
2017-06-18 22:04:19 -0300

    Mimic Image behaviour and add proptypes

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Animated, Easing } from 'react-native';
+import PropTypes from 'prop-types'
 
 export default class Spinner extends React.Component {
   state = {
@@ -37,7 +38,12 @@ export default class Spinner extends React.Component {
         style={[{
           transform: [{rotate: spin}]
         }, this.props.style]}
-        source={this.props.image} />
+        source={this.props.source} />
     );
   }
+}
+
+Spinner.propTypes = {
+  style: PropTypes.number.isRequired,
+  source: PropTypes.number.isRequired,
 }
